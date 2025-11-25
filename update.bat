8ad9488903986f5d0ce0103accad40aa5af07da4
Jan Knobloch <knobloch@vutbr.cz>
2025-11-25 16:30:59 +0100

    Upraven update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,10 @@
+@echo off
+:: Skriptik provede:
+:: * Smaze vsechny soubory, ktere nejsou pod kontrolou git
+:: * Vrati vsechny zmeny na posledni commit
+:: * Updatuje vse ze serveru
+::
 git clean -fd
+git restore .
 git pull
 pause
