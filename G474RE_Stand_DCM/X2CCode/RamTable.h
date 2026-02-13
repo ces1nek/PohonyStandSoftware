88ddcf037385ac4f1dcc6016045ceb2da4f0256b
DESKTOP-HBM9VE0\adminLab <PohonySA14_ST9>
2026-02-13 10:50:44 +0100

    Pridana simulace rizeni

--- G474RE_Stand_DCM/X2CCode/RamTable.h
+++ G474RE_Stand_DCM/X2CCode/RamTable.h
@@ -2,14 +2,14 @@
  * @file
  * @brief Generated RAM table file.
  * 
- * Date:  2026-02-13 10:26
+ * Date:  2026-02-13 10:48
  * 
  * X2C-Version: 6.4.2961
  * X2C-Edition: Educational
  */
 /* This file is part of X2C. http://x2c.lcm.at/                                                                       */
 
-/* Date:  2026-02-13 10:26                                                                                            */
+/* Date:  2026-02-13 10:48                                                                                            */
 
 /* X2C-Version: 6.4.2961                                                                                              */
 /* X2C-Edition: Educational                                                                                           */
