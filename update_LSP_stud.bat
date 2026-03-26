16ef457f39f2a79e33391b745d6b11436671aa45
DESKTOP-HBM9VE0\adminLab <PohonySA14_ST9>
2026-03-26 13:24:39 +0100

    Pridan update_LSP

---
+++ update_LSP_stud.bat
@@ -0,0 +1,11 @@
+@echo off
+:: Skriptik provede:
+:: * Smaze vsechny soubory, ktere nejsou pod kontrolou git
+:: * Vrati vsechny zmeny na posledni commit
+:: * Updatuje vse ze serveru
+::
+git clean -fd
+git restore .
+git pull
+git checkout "StudentVerForDownload"
+pause
